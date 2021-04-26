125596142db89eeb67e4e4c74b57f1ff8a8253de
Wenning Ding <wenningd@amazon.com>
2021-04-26 12:06:56 -0700

    refactor LockConfiguration

--- hudi-common/src/main/java/org/apache/hudi/common/config/LockConfiguration.java
+++ hudi-common/src/main/java/org/apache/hudi/common/config/LockConfiguration.java
@@ -28,36 +28,126 @@ import java.util.Properties;
 public class LockConfiguration implements Serializable {
 
   public static final String LOCK_PREFIX = "hoodie.write.lock.";
-  public static final String LOCK_ACQUIRE_RETRY_WAIT_TIME_IN_MILLIS_PROP = LOCK_PREFIX + "wait_time_ms_between_retry";
-  public static final String DEFAULT_LOCK_ACQUIRE_RETRY_WAIT_TIME_IN_MILLIS = String.valueOf(5000L);
-  public static final String LOCK_ACQUIRE_RETRY_MAX_WAIT_TIME_IN_MILLIS_PROP = LOCK_PREFIX + "max_wait_time_ms_between_retry";
-  public static final String DEFAULT_LOCK_ACQUIRE_MAX_RETRY_WAIT_TIME_IN_MILLIS = String.valueOf(5000L);
-  public static final String LOCK_ACQUIRE_CLIENT_RETRY_WAIT_TIME_IN_MILLIS_PROP = LOCK_PREFIX + "client.wait_time_ms_between_retry";
-  public static final String DEFAULT_LOCK_ACQUIRE_CLIENT_RETRY_WAIT_TIME_IN_MILLIS = String.valueOf(10000L);
-  public static final String LOCK_ACQUIRE_NUM_RETRIES_PROP = LOCK_PREFIX + "num_retries";
-  public static final String DEFAULT_LOCK_ACQUIRE_NUM_RETRIES = String.valueOf(3);
-  public static final String LOCK_ACQUIRE_CLIENT_NUM_RETRIES_PROP = LOCK_PREFIX + "client.num_retries";
-  public static final String DEFAULT_LOCK_ACQUIRE_CLIENT_NUM_RETRIES = String.valueOf(0);
-  public static final String LOCK_ACQUIRE_WAIT_TIMEOUT_MS_PROP = LOCK_PREFIX + "wait_time_ms";
-  public static final int DEFAULT_ACQUIRE_LOCK_WAIT_TIMEOUT_MS = 60 * 1000;
+
+  public static final ConfigOption<String> LOCK_ACQUIRE_RETRY_WAIT_TIME_IN_MILLIS_PROP = ConfigOption
+      .key(LOCK_PREFIX + "wait_time_ms_between_retry")
+      .defaultValue(String.valueOf(5000L))
+      .withDescription("");
+
+  public static final ConfigOption<String> LOCK_ACQUIRE_RETRY_MAX_WAIT_TIME_IN_MILLIS_PROP = ConfigOption
+      .key(LOCK_PREFIX + "max_wait_time_ms_between_retry")
+      .defaultValue(String.valueOf(5000L))
+      .withDescription("");
+
+  public static final ConfigOption<String> LOCK_ACQUIRE_CLIENT_RETRY_WAIT_TIME_IN_MILLIS_PROP = ConfigOption
+      .key(LOCK_PREFIX + "client.wait_time_ms_between_retry")
+      .defaultValue(String.valueOf(10000L))
+      .withDescription("");
+
+  public static final ConfigOption<String> LOCK_ACQUIRE_NUM_RETRIES_PROP = ConfigOption
+      .key(LOCK_PREFIX + "num_retries")
+      .defaultValue(String.valueOf(3))
+      .withDescription("");
+
+  public static final ConfigOption<String> LOCK_ACQUIRE_CLIENT_NUM_RETRIES_PROP = ConfigOption
+      .key(LOCK_PREFIX + "client.num_retries")
+      .defaultValue(String.valueOf(0))
+      .withDescription("");
+
+  public static final ConfigOption<Integer> LOCK_ACQUIRE_WAIT_TIMEOUT_MS_PROP = ConfigOption
+      .key(LOCK_PREFIX + "wait_time_ms")
+      .defaultValue(60 * 1000)
+      .withDescription("");
+
   // configs for file system based locks. NOTE: This only works for DFS with atomic create/delete operation
   public static final String FILESYSTEM_BASED_LOCK_PROPERTY_PREFIX = LOCK_PREFIX + "filesystem.";
-  public static final String FILESYSTEM_LOCK_PATH_PROP = FILESYSTEM_BASED_LOCK_PROPERTY_PREFIX + "path";
+
+  public static final ConfigOption<String> FILESYSTEM_LOCK_PATH_PROP = ConfigOption
+      .key(FILESYSTEM_BASED_LOCK_PROPERTY_PREFIX + "path")
+      .noDefaultValue()
+      .withDescription("");
+
   // configs for metastore based locks
   public static final String HIVE_METASTORE_LOCK_PROPERTY_PREFIX = LOCK_PREFIX + "hivemetastore.";
-  public static final String HIVE_DATABASE_NAME_PROP = HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "database";
-  public static final String HIVE_TABLE_NAME_PROP = HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "table";
-  public static final String HIVE_METASTORE_URI_PROP = HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "uris";
+
+  public static final ConfigOption<String> HIVE_DATABASE_NAME_PROP = ConfigOption
+      .key(HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "database")
+      .noDefaultValue()
+      .withDescription("");
+
+  public static final ConfigOption<String> HIVE_TABLE_NAME_PROP = ConfigOption
+      .key(HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "table")
+      .noDefaultValue()
+      .withDescription("");
+
+  public static final ConfigOption<String> HIVE_METASTORE_URI_PROP = ConfigOption
+      .key(HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "uris")
+      .noDefaultValue()
+      .withDescription("");
+
   // Zookeeper configs for zk based locks
   public static final String ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX = LOCK_PREFIX + "zookeeper.";
-  public static final String ZK_BASE_PATH_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "base_path";
-  public static final String ZK_SESSION_TIMEOUT_MS_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "session_timeout_ms";
-  public static final int DEFAULT_ZK_SESSION_TIMEOUT_MS = 60 * 1000;
-  public static final String ZK_CONNECTION_TIMEOUT_MS_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "connection_timeout_ms";
-  public static final int DEFAULT_ZK_CONNECTION_TIMEOUT_MS = 15 * 1000;
-  public static final String ZK_CONNECT_URL_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "url";
-  public static final String ZK_PORT_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "port";
-  public static final String ZK_LOCK_KEY_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "lock_key";
+
+  public static final ConfigOption<String> ZK_BASE_PATH_PROP = ConfigOption
+      .key(ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "base_path")
+      .noDefaultValue()
+      .withDescription("");
+
+  public static final ConfigOption<Integer> ZK_SESSION_TIMEOUT_MS_PROP = ConfigOption
+      .key(ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "session_timeout_ms")
+      .defaultValue(60 * 1000)
+      .withDescription("");
+
+  public static final ConfigOption<Integer> ZK_CONNECTION_TIMEOUT_MS_PROP = ConfigOption
+      .key(ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "connection_timeout_ms")
+      .defaultValue(15 * 1000)
+      .withDescription("");
+
+  public static final ConfigOption<String> ZK_CONNECT_URL_PROP = ConfigOption
+      .key(ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "url")
+      .noDefaultValue()
+      .withDescription("");
+
+  public static final ConfigOption<String> ZK_PORT_PROP = ConfigOption
+      .key(ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "port")
+      .noDefaultValue()
+      .withDescription("");
+
+  public static final ConfigOption<String> ZK_LOCK_KEY_PROP = ConfigOption
+      .key(ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "lock_key")
+      .noDefaultValue()
+      .withDescription("");
+
+  // public static final String LOCK_ACQUIRE_RETRY_WAIT_TIME_IN_MILLIS_PROP = LOCK_PREFIX + "wait_time_ms_between_retry";
+  // public static final String DEFAULT_LOCK_ACQUIRE_RETRY_WAIT_TIME_IN_MILLIS = String.valueOf(5000L);
+  // public static final String LOCK_ACQUIRE_RETRY_MAX_WAIT_TIME_IN_MILLIS_PROP = LOCK_PREFIX + "max_wait_time_ms_between_retry";
+  // public static final String DEFAULT_LOCK_ACQUIRE_MAX_RETRY_WAIT_TIME_IN_MILLIS = String.valueOf(5000L);
+  // public static final String LOCK_ACQUIRE_CLIENT_RETRY_WAIT_TIME_IN_MILLIS_PROP = LOCK_PREFIX + "client.wait_time_ms_between_retry";
+  // public static final String DEFAULT_LOCK_ACQUIRE_CLIENT_RETRY_WAIT_TIME_IN_MILLIS = String.valueOf(10000L);
+  // public static final String LOCK_ACQUIRE_NUM_RETRIES_PROP = LOCK_PREFIX + "num_retries";
+  // public static final String DEFAULT_LOCK_ACQUIRE_NUM_RETRIES = String.valueOf(3);
+  // public static final String LOCK_ACQUIRE_CLIENT_NUM_RETRIES_PROP = LOCK_PREFIX + "client.num_retries";
+  // public static final String DEFAULT_LOCK_ACQUIRE_CLIENT_NUM_RETRIES = String.valueOf(0);
+  // public static final String LOCK_ACQUIRE_WAIT_TIMEOUT_MS_PROP = LOCK_PREFIX + "wait_time_ms";
+  // public static final int DEFAULT_ACQUIRE_LOCK_WAIT_TIMEOUT_MS = 60 * 1000;
+  // configs for file system based locks. NOTE: This only works for DFS with atomic create/delete operation
+  // public static final String FILESYSTEM_BASED_LOCK_PROPERTY_PREFIX = LOCK_PREFIX + "filesystem.";
+  // public static final String FILESYSTEM_LOCK_PATH_PROP = FILESYSTEM_BASED_LOCK_PROPERTY_PREFIX + "path";
+  // configs for metastore based locks
+  // public static final String HIVE_METASTORE_LOCK_PROPERTY_PREFIX = LOCK_PREFIX + "hivemetastore.";
+  // public static final String HIVE_DATABASE_NAME_PROP = HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "database";
+  // public static final String HIVE_TABLE_NAME_PROP = HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "table";
+  // public static final String HIVE_METASTORE_URI_PROP = HIVE_METASTORE_LOCK_PROPERTY_PREFIX + "uris";
+  // Zookeeper configs for zk based locks
+  // public static final String ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX = LOCK_PREFIX + "zookeeper.";
+  // public static final String ZK_BASE_PATH_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "base_path";
+  // public static final String ZK_SESSION_TIMEOUT_MS_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "session_timeout_ms";
+  // public static final int DEFAULT_ZK_SESSION_TIMEOUT_MS = 60 * 1000;
+  // public static final String ZK_CONNECTION_TIMEOUT_MS_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "connection_timeout_ms";
+  // public static final int DEFAULT_ZK_CONNECTION_TIMEOUT_MS = 15 * 1000;
+  // public static final String ZK_CONNECT_URL_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "url";
+  // public static final String ZK_PORT_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "port";
+  // public static final String ZK_LOCK_KEY_PROP = ZOOKEEPER_BASED_LOCK_PROPERTY_PREFIX + "lock_key";
 
   private final TypedProperties props;
 
